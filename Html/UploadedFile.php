b27a316dc840918e0f2f320e18ee3d0fd3ab520e
tyc <tyc@TYC.shoptet.local>
2013-07-31 17:14:32 +0200

    Removed some "universal" library dependencies

--- Html/UploadedFile.php
+++ Html/UploadedFile.php
@@ -1,34 +1,50 @@
 <?php
 namespace RqData\Html;
 
-class UploadedFile {
+class UploadedFile extends \RqData\Core\Object {
+
+	private $inputName;
+	private $currentFilepath;
+	private $tempFilepath;
+	private $name;
 
 	/**
 	 * tempName ?
 	 */
 	public function __construct($inputName) {
-		$this->setInputName($inputName);
+		$this->inputName = $inputName;
+		$this->tempFilepath = $_FILES[$this->inputName]['tmp_name'];
+		$this->currentFilepath = $this->tempFilepath;
+		$this->name = $_FILES[$this->inputName]['name'];
 	}
 
 	public function moveTo($destinationFilepath) {
-		if (!move_uploaded_file($this->tempName , $destinationFilepath)) {
+		if (!move_uploaded_file($this->currentFilepath , $destinationFilepath)) {
 			throw new \RqData\File\Exceptions\CopyOfFileFailed(sprintf('Destination [%s]', $destinationFilepath));
+		} else {
+			$this->currentFilepath = $destinationFilepath;
 		}
 	}
 
 	public function getSize() {
-
+		return filesize($this->currentFilepath);
 	}
 
 	public function getName() {
-
+		return $this->name;
 	}
 
 	public function getContentArray() {
+		$resource = fopen($this->currentFilepath, 'r');
+		$content = array();
+		while (!feof($resource)) {
+			$content[] = fgets($resource);
+		}
 
+		return $content;
 	}
 
 	public function getTempFilename() {
-
+		return basename($this->tempFilepath);
 	}
 }
--- Process/ResultFileDownloader.php
+++ Process/ResultFileDownloader.php
@@ -4,6 +4,13 @@ namespace RqData\Process;
 use RqData\Registry\UserErrors;
 
 class ResultFileDownloader extends Base {
+
+	const SIZE_UNIT_KB = 1024; //2e10
+	const CONTENT_TYPE_PLAINT_TEXT = 'plain/text';
+	const CONTENT_TYPE_TEXT_JAVASCRIPT = 'type="text/javascript"';
+	const CONTENT_TYPE_EXCEL = 'application/vnd.ms-excel';
+	const DEFAULT_CONTENT_TYPE = self::CONTENT_TYPE_PLAINT_TEXT;
+
 	private $result;
 
 	public function __construct(Result $result, UserErrors $errors){
@@ -17,10 +24,34 @@ class ResultFileDownloader extends Base {
 	}
 
 	protected function offerFileDownload() {
-		\universal\Folder\Utilities\HtmlUtilities::download(
-			$this->getResult()->getFilename(),
-			$this->getResult()->getUserFileBasename()
-		);
+		$resource = fopen($this->getResult()->getFilename(), 'r');
+		$this->sendFileDownloadHeader($this->getResult()->getUserFileBasename());
+		while (!feof($resource)) {
+			echo fgets($resource, self::SIZE_UNIT_KB);
+		}
+	}
+
+	private function sendFileDownloadHeader($downloadedFilename, $originalFilepath) {
+		$suffix = array();
+		preg_match('~\.[^.]+$', $downloadedFilename, $suffix);
+		if (empty($suffix[0])) {
+			$contentType = self::DEFAULT_CONTENT_TYPE;
+		} else {
+			switch($suffix[0]) {
+				case 'xls':
+					$contentType = self::CONTENT_TYPE_EXCEL;
+					break;
+				case 'txt':
+					$contentType = self::CONTENT_TYPE_PLAINT_TEXT;
+					break;
+				default:
+					$contentType = self::DEFAULT_CONTENT_TYPE;
+			}
+		}
+
+		header('Content-Type: ' . $contentType);
+		header('Content-Disposition: attachment;filename="' . urlencode($downloadedFilename) . '"');
+		header('Content-Length: ' . filesize($originalFilepath));
 	}
 
 	/**
