3d91048f02e234f5ef110fff6011f2aac7fe6b1c
tyc <tyc@TYC.shoptet.local>
2013-08-02 08:17:26 +0200

    bugfix: Core object can be used

--- Core/Object.php
+++ Core/Object.php
@@ -3,22 +3,22 @@ namespace RqData\Core;
 
 abstract class Object {
 	public function __get($name) {
-		throw new \RqData\Core\Exceptions\ReadingAccess(sprintf('Reading of variable [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
+		throw new \RqData\Core\Exceptions\ReadingAccess(sprintf('Reading of property [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 
-	public function __set($name) {
-		throw new \RqData\Core\Exceptions\WrittingAccess(sprintf('Writting to variable [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
+	public function __set($name, $value) {
+		throw new \RqData\Core\Exceptions\WrittingAccess(sprintf('Writting to property [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 
 	public function __isset($name) {
-		throw new \RqData\Core\Exceptions\ReadingAccess(sprintf('Checking variable [%s->%s] if is set fails. Does not exists or has restricted access.', \get_class($this), $name));
+		throw new \RqData\Core\Exceptions\ReadingAccess(sprintf('Checking property [%s->%s] if is set fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 
-	public function __call($name) {
-		throw new \RqData\Core\Exceptions\ExecutingAccess(sprintf('Executing function [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
+	public function __call($name, $parameters) {
+		throw new \RqData\Core\Exceptions\ExecutingAccess(sprintf('Executing method [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 
-	public function __callStatic($name) {
-		throw new \RqData\Core\Exceptions\ExecutingAccess(sprintf('Executing static function [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
+	public static function __callStatic($name, $parameters) {
+		throw new \RqData\Core\Exceptions\ExecutingAccess(sprintf('Executing static method [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 }
