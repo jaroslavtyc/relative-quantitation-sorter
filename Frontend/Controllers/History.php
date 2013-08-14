0e5bae75a4c2fab1819fef50a2722b7ba8f627cb
tyc <tyc@TYC.shoptet.local>
2013-08-14 14:54:50 +0200

    History content is shown

--- Frontend/Controllers/History.php
+++ Frontend/Controllers/History.php
@@ -4,11 +4,11 @@ namespace RqData\Frontend\Controllers;
 use RqData\History\File;
 
 class History extends DisplayWithErrorMessages {
-	protected function setUpFetcher() {
-		parent::setUpFetcher();
+	public function display() {
 		$this->getFetcher()->assign('history', $this->buildHistoryFile());
 		$this->getFetcher()->assign('css', array('main.css','history.css','system/main.css'));
 		$this->getFetcher()->assign('headerJs', array('libraries/jquery.js'));
+		parent::display();
 	}
 
 	protected function buildHistoryFile() {
