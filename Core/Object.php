d5e5c6b442f30e65e8c49ae9889f9c1e4d02265f
Jaroslav Tyc <tyc@shoptet.cz>
2014-04-02 18:15:19 +0200

    Shortened class names because of used namespace.

--- Core/Object.php
+++ Core/Object.php
@@ -3,22 +3,22 @@ namespace RqData\Core;
 
 abstract class Object {
 	public function __get($name) {
-		throw new \RqData\Core\Exceptions\ReadingAccess(sprintf('Reading of property [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
+		throw new Exceptions\ReadingAccess(sprintf('Reading of property [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 
 	public function __set($name, $value) {
-		throw new \RqData\Core\Exceptions\WrittingAccess(sprintf('Writting to property [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
+		throw new Exceptions\WrittingAccess(sprintf('Writting to property [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 
 	public function __isset($name) {
-		throw new \RqData\Core\Exceptions\ReadingAccess(sprintf('Checking property [%s->%s] if is set fails. Does not exists or has restricted access.', \get_class($this), $name));
+		throw new Exceptions\ReadingAccess(sprintf('Checking property [%s->%s] if is set fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 
 	public function __call($name, $parameters) {
-		throw new \RqData\Core\Exceptions\ExecutingAccess(sprintf('Executing method [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
+		throw new Exceptions\ExecutingAccess(sprintf('Executing method [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
 	}
 
 	public static function __callStatic($name, $parameters) {
-		throw new \RqData\Core\Exceptions\ExecutingAccess(sprintf('Executing static method [%s->%s] fails. Does not exists or has restricted access.', \get_class($this), $name));
+		throw new Exceptions\ExecutingAccess(sprintf('Executing static method [%s->%s] fails. Does not exists or has restricted access.', \get_called_class(), $name));
 	}
 }
